ca4556aff9625e85405ecf7250f0e647f507791a
neeleshpoli <72574589+neeleshpoli@users.noreply.github.com>
2025-07-21 18:52:54 -0500

    - Add temporaray KV storage so that worker doesn't hit upload limits and images can be stored temporarily somewhere else

--- public/src/menu/image.rs
+++ public/src/menu/image.rs
@@ -1,41 +1,53 @@
-use axum::{body::Bytes, extract::Path};
+use axum::{
+    body::Bytes,
+    extract::{Path, State},
+};
+use worker::Env;
 
 use crate::error::ApiResult;
 
-const IDLI: &[u8] = include_bytes!("test_images/idli.jpg");
-const KADAPA_GHEE_KARAM_DOSA: &[u8] = include_bytes!("test_images/Kadapa Ghee Karam Dosa.jpg");
-const KADAPA_KARAM_DOSA: &[u8] = include_bytes!("test_images/Kadapa Karam Dosa.jpg");
-const MASALA_DOSA: &[u8] = include_bytes!("test_images/Masala Dosa.png");
-const ONION_DOSA: &[u8] = include_bytes!("test_images/Onion Dosa.jpg");
-const ONION_MASALA_DOSA: &[u8] = include_bytes!("test_images/Onion Masala Dosa.jpg");
-const PLAIN_DOSA: &[u8] = include_bytes!("test_images/Plain Dosa.png");
+// const IDLI: &[u8] = include_bytes!("test_images/idli.jpg");
+// const KADAPA_GHEE_KARAM_DOSA: &[u8] = include_bytes!("test_images/Kadapa Ghee Karam Dosa.jpg");
+// const KADAPA_KARAM_DOSA: &[u8] = include_bytes!("test_images/Kadapa Karam Dosa.jpg");
+// const MASALA_DOSA: &[u8] = include_bytes!("test_images/Masala Dosa.png");
+// const ONION_DOSA: &[u8] = include_bytes!("test_images/Onion Dosa.jpg");
+// const ONION_MASALA_DOSA: &[u8] = include_bytes!("test_images/Onion Masala Dosa.jpg");
+// const PLAIN_DOSA: &[u8] = include_bytes!("test_images/Plain Dosa.png");
 
 /// Get the full resolution image
 #[worker::send]
 #[axum::debug_handler]
-pub async fn get_image(Path(id): Path<usize>) -> ApiResult<Bytes> {
-    Ok(Bytes::from(match id {
-        2 => PLAIN_DOSA,
-        3 => ONION_DOSA,
-        4 => MASALA_DOSA,
-        5 => ONION_MASALA_DOSA,
-        6 => KADAPA_KARAM_DOSA,
-        7 => KADAPA_GHEE_KARAM_DOSA,
-        _ => IDLI,
-    }))
+pub async fn get_image(State(env): State<Env>, Path(id): Path<usize>) -> ApiResult<Bytes> {
+    // TODO: Properly get image
+    Ok(Bytes::from(
+        env.kv("images-temp")?
+            .get(&id.to_string())
+            .bytes()
+            .await
+            .unwrap()
+            .unwrap(),
+    ))
 }
 
 /// Get the 100x100 resolution image
 #[worker::send]
 #[axum::debug_handler]
-pub async fn get_thumbnail(Path(id): Path<usize>) -> ApiResult<Bytes> {
-    Ok(Bytes::from(match id {
-        2 => PLAIN_DOSA,
-        3 => ONION_DOSA,
-        4 => MASALA_DOSA,
-        5 => ONION_MASALA_DOSA,
-        6 => KADAPA_KARAM_DOSA,
-        7 => KADAPA_GHEE_KARAM_DOSA,
-        _ => IDLI,
-    }))
+pub async fn get_thumbnail(State(env): State<Env>, Path(id): Path<usize>) -> ApiResult<Bytes> {
+    // Temporarily add every image (so that image storage doesn't cost anything)
+    // env.kv("images-temp")?.put_bytes("1", IDLI).unwrap().execute().await.unwrap();
+    // env.kv("images-temp")?.put_bytes("2", PLAIN_DOSA).unwrap().execute().await.unwrap();
+    // env.kv("images-temp")?.put_bytes("3", ONION_DOSA).unwrap().execute().await.unwrap();
+    // env.kv("images-temp")?.put_bytes("4", MASALA_DOSA).unwrap().execute().await.unwrap();
+    // env.kv("images-temp")?.put_bytes("5", ONION_MASALA_DOSA).unwrap().execute().await.unwrap();
+    // env.kv("images-temp")?.put_bytes("6", KADAPA_KARAM_DOSA).unwrap().execute().await.unwrap();
+    // env.kv("images-temp")?.put_bytes("7", KADAPA_GHEE_KARAM_DOSA).unwrap().execute().await.unwrap();
+
+    Ok(Bytes::from(
+        env.kv("images-temp")?
+            .get(&id.to_string())
+            .bytes()
+            .await
+            .unwrap()
+            .unwrap(),
+    ))
 }
