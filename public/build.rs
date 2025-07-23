2ad185efb1a83e9ec98dfbd8e517754d621e33c3
neeleshpoli <72574589+neeleshpoli@users.noreply.github.com>
2025-07-23 09:13:35 -0500

    - Fix temporary directory

--- public/build.rs
+++ public/build.rs
@@ -1,7 +1,7 @@
 use std::{env, process::{Command, Stdio}};
 
 fn main() {
-    let web_build = Command::new("flutter")
+    Command::new("flutter")
         .args(["build", "web", "--wasm", "--release"])
         .current_dir("./frontend/")
         .stdout(Stdio::inherit())
@@ -11,14 +11,14 @@ fn main() {
 
     if env::var("NON_ROOT_CWD").is_ok() {
         Command::new("mkdir")
-            .args(["-p", "../backend/public/frontend/build/web"])
+            .args(["-p", "../backend/public/frontend/build"])
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
             .status()
             .expect("Failed to execute temp directory creation!");
 
         Command::new("mv")
-            .args(["./frontend/build/web", "../backend/public/frontend/build/web"])
+            .args(["./frontend/build/web", "../backend/public/frontend/build"])
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
             .status()
