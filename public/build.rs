2252917fc2fa71079ec3aa48c2fb1423ad977ffa
neeleshpoli <72574589+neeleshpoli@users.noreply.github.com>
2025-07-22 20:43:20 -0500

    - Add workaround for directory not found for github actions

--- public/build.rs
+++ public/build.rs
@@ -1,11 +1,30 @@
-use std::process::{Command, Stdio};
+use std::{
+    env,
+    process::{Command, Stdio},
+};
 
 fn main() {
-    let web_build = Command::new("flutter")
+    let _ = Command::new("flutter")
         .args(["build", "web", "--wasm", "--release"])
         .current_dir("./frontend/")
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .status()
         .expect("Failed to execute flutter web build command!");
+
+    if env::var("NON_ROOT_CWD").is_ok() {
+        Command::new("mkdir")
+            .args(["-p", "../frontend/build/"])
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .status()
+            .expect("Failed to execute temp directory creation!");
+
+        Command::new("mv")
+            .args(["./frontend/build/web", "../frontend/build/"])
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .status()
+            .expect("Failed to execute temporary move!");
+    }
 }
